cd23ec2bd0cf5e272af980b87e68c010da9199d1
Tim Evans <tevans@movableink.com>
2016-03-08 15:37:11 -0500

    remove ability to invoke return; seems weird

--- tests/unit/index-test.js
+++ tests/unit/index-test.js
@@ -3,7 +3,7 @@ import V$ from 'virtual-jquery';
 module('virtual-jquery');
 
 test('toString returns the HTML serialized back to a string', function (assert) {
-  let $ = V$(`
+  let $html = V$(`
     <!DOCTYPE html>
     <html>
       <head>
@@ -13,11 +13,11 @@ test('toString returns the HTML serialized back to a string', function (assert) 
     </html>
   `);
 
-  assert.equal($.toString().replace(/\n\s+/g, ''), `<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head><title>Hello</title></head><body></body></html>`);
+  assert.equal($html.toString().replace(/\n\s+/g, ''), `<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head><title>Hello</title></head><body></body></html>`);
 });
 
 test('find() returns the element that matches the selector', function (assert) {
-  let $ = V$(`
+  let $html = V$(`
     <!DOCTYPE html>
     <html>
       <head>
@@ -27,23 +27,7 @@ test('find() returns the element that matches the selector', function (assert) {
     </html>
   `);
 
-  let title = $.find('title');
-  assert.equal(title.length, 1);
-  assert.equal(title[0].tagName, 'title');
-});
-
-test('invoking the return value is the same as find()', function (assert) {
-  let $ = V$(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Hello</title>
-      </head>
-      <body></body>
-    </html>
-  `);
-
-  let title = $.find('title');
+  let title = $html.find('title');
   assert.equal(title.length, 1);
   assert.equal(title[0].tagName, 'title');
 });
